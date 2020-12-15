6bffd9492ca15efe4bf04c653f8f097c15325c02
gurbuzertunga <gurbuzertunga@gmail.com>
2020-12-15 18:19:34 +0300

    setup tailwind

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import './styles.css';
+
+
+const div = document.createElement('div');
+div.textContent = 'Test me';
+div.classList.add('text-red-500');
+document.body.appendChild(div);
 console.log('TEST');
