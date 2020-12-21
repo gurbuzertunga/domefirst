d08642baa61e935dabd2797f1425ff9998ed5b94
vanheaven <vanheaven6@gmail.com>
2020-12-21 12:20:45 +0300

    Configure postcss

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,11 @@ module.exports = {
         use: [
           'style-loader',
           'css-loader',
+          {
+            loader: 'css-loader', options: { importLoaders: 1 }
+          },
+
+          'postcss-loader',
         ],
       },
     ],
